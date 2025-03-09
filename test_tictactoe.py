5b8e0809f82c36352ed3a7becf4513ea2f6c19ca
Kesava1995 <nvpkesava@gmail.com>
2025-03-09 14:40:53 +0530

    Create test_tictactoe.py

---
+++ test_tictactoe.py
@@ -0,0 +1,15 @@
+import unittest
+from TicTacToe import checkWin  # Import your functions from the main script
+
+class TestTicTacToe(unittest.TestCase):
+    def test_win_conditions(self):
+        """Test winning conditions for X and O"""
+        self.assertTrue(checkWin([["X", "X", "X"], [0, "O", 0], ["O", 0, "O"]]))
+        self.assertTrue(checkWin([["O", "O", "O"], ["X", "X", 0], [0, "X", 0]]))
+
+    def test_no_win(self):
+        """Test for no win scenario"""
+        self.assertFalse(checkWin([["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]))
+
+if __name__ == "__main__":
+    unittest.main()
